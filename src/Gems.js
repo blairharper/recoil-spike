1d6f8511f4eebff32594559266b6496b8d0b86b7
Blair Harper <blair@passio.co.uk>
2020-05-18 11:18:25 +0100

    game with click score and gems

---
+++ src/Gems.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { useRecoilValue } from "recoil";
+
+import { gameScore } from "./atoms";
+
+const Gems = () => {
+  const score = useRecoilValue(gameScore);
+
+  return (
+    <div>
+      {[...Array(score)].map((n, i) => {
+        return <span key={i}>💎</span>;
+      })}
+    </div>
+  );
+};
+
+export default Gems;
